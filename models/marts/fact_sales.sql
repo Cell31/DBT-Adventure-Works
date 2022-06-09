c3b548011d9e3f16ad61d6614fa50afd9e1908b1
celso.lemos@q2pay.com.br <celso.lemos@q2pay.com.br>
2022-06-09 13:19:04 +0000

    save

--- models/marts/fact_sales.sql
+++ models/marts/fact_sales.sql
@@ -6,13 +6,8 @@ with detail as (
     select * 
     from {{ref('stg_sales_salesorderheader')}}
 )
-    , sales as (
-    select detail.salesorderid,
-           detail.salesorderdetailid,
-           detail.orderqty,
-           detail.productid, 
-           header.orderdate,
-           header.customerid
+   , sales as (
+    select *
     FROM detail
     left join header on
        detail.salesorderid = 
